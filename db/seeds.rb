0f689ec947e6f1a90fc5906f78302969fe0472a2
Victoria <vic.chen128@gmail.com>
2017-06-29 15:47:26 -0700

    Add seed file using faker

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+
+num_users = 5
+
+num_users.times do |n|
+  user1 = User.create(username: Faker::Name.name, password: 'password')
+  user2 = User.create(username: Faker::Name.name, password: 'password')
+
+  # first user asks question
+  user1.questions.create(title: "#{user1.username} Question Title", body: "#{user1.username} Question Body")
+
+  # second user answers that question
+  user2.answers.create(body: "#{user2.username} Answer Body", question_id: Question.last, best_answer: false )
+end
+
