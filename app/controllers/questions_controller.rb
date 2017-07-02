e936b49b6ea23038b577621162015effa521b190
Victoria <vic.chen128@gmail.com>
2017-07-03 01:40:03 +0800

    Ractor out references to @best_answer and @other_answers in controllers to reduce code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,5 @@
 get '/questions/:question_id' do
   @question = Question.find(params[:question_id])
-  # @best_answer = @question.best_answer
-  # @other_answers = @question.other_answers
   erb :'/questions/show'
 end
 
